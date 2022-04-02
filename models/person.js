1dbf87df351ff332f1caa2d43e2db45086e5c790
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-04-02 12:43:28 -0500

    crear modelo

---
+++ models/person.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Shema = mongoose.Schema;
+
+const PersonSherma = Shema({
+    name:String,
+    image:String,
+    age:Number
+})
+//ELEGIR ESQUEMA Y ELGIRLO COMO MODELO nombre de la tabla Primera mayucula, luego en singular, el esquema
+module.exports = mongoose.model('Person',PersonSherma)

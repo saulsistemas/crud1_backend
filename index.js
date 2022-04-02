4b1eb524a91c70fc83b7b23622b62225e87ae702
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-04-01 20:55:34 -0500

    conectandonos a bd

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hola mundo');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://0.0.0.0:27017/bdcrud')
+        .then(function(){
+            console.log('conectado correctamente');
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+

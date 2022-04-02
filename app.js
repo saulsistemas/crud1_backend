84e2d52d90beea462c5595e7891b0902f2279259
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-04-02 12:04:42 -0500

    configurando body-parser

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser')//3 CONFIGURAR BODY PARSER;
 const app = express(); //INSTANCIA DE MODULO
-//VALIDAR QUE EXPREES FUNCIONA
-//const port = 3700;
-//app.listen(port,function(){
-//    console.log('servidor creado correctamente ' +port);
-//})
-//
+
+//CARGAR ARCHIVOS DE RUTA
+
+//MIDELWARES
+app.use(bodyParser.urlencoded({extended:false})) //3 PERMITE CONVERTIR TODO CONTENIDO QUE LLEGUE POR CUERTO A JSON
+app.use(bodyParser.json());
+
+//CORDS
+
+
+//RUTA
+
 app.get('/',function(request,response){
     response.status(200).send({
         mensaje:"todo bien"
